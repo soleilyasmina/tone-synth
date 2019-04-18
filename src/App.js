710d590b2a113fed7658ce17f39ff5df8e7a2579
soleilyasmina <mounlasolomon@gmail.com>
2019-04-18 11:22:36 -0400

    started tone

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={() => console.log('test')}>TONE</button>
+        <button onClick={() => Tone.start()}>TONE</button>
       </div>
     );
   }
