3d42d2c9499a57d8b854037d2daed31bd4e57e2a
soleilyasmina <mounlasolomon@gmail.com>
2019-04-21 16:43:13 -0400

    got synth to play!

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import Tone from 'tone';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      synth: null
+    }
+  }
+  start = () => {
+    Tone.start();
+    let synth = new Tone.PolySynth({
+	"oscillator" : {
+		"type" : "pwm",
+		"modulationFrequency" : 0.2
+	},
+	"envelope" : {
+		"attack" : 0.02,
+		"decay" : 0.1,
+		"sustain" : 0.2,
+		"release" : 0.1,
+	}
+    }).toMaster();
+    synth.triggerAttack("D3", "+1");
+    
+    this.setState({synth});
+  }
+  render = () => {
     return (
       <div className="App">
-        <button onClick={() => Tone.start()}>TONE</button>
+        <button onClick={this.start}>TONE</button>
       </div>
     );
   }
